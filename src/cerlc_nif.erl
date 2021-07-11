b0acf8700dc9b6cde9d667776dc69e539cee0fb2
mdsebald <mdsebald@gmail.com>
2021-07-11 08:38:09 -0500

    Change load_nif() path

--- src/cerlc_nif.erl
+++ src/cerlc_nif.erl
@@ -9,7 +9,19 @@
 -on_load(load_nif/0).
 
 % Attempt to load the 'C' NIF implementation of this library
-load_nif() -> erlang:load_nif("./priv/cerlc_nif", 0).
+load_nif() ->
+  SoName = get_priv_path(?MODULE),
+  io:format(<<"Loading library: ~p ~n">>, [SoName]),
+  ok = erlang:load_nif(SoName, 0).
+
+get_priv_path(File) ->
+  case code:priv_dir(cerlc_nif) of
+      {error, bad_name} ->
+          Ebin = filename:dirname(code:which(?MODULE)),
+          filename:join([filename:dirname(Ebin), "priv", File]);
+      Dir ->
+          filename:join(Dir, File)
+  end.
 
 % @doc
 % Generate a record containing the CRC calculation function
